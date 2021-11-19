e16ee40e5ad8501f9fbe275ab0f2b54c60cefa8f
Mezdelex <alexcondegomez@gmail.com>
2021-11-19 16:50:02 +0100

    compiler test

--- compiler_test.cpp
+++ compiler_test.cpp
@@ -5,7 +5,7 @@ main()
     std::cout << "This is a compiler test" << std::endl;
 }
 
-// Standalone cpp compiler (debugger, etc.) to work with VS Code without any VS installation needed.
+// Standalone cpp compiler (debugger, etc.) to work with VS Code without any VS installation.
 // Download the .zip from latest releases, unzip it and add the folder 'bin' to Windows PATH variable.
 // You can use Powershell or whatever shell you want, no need to use the embeded VS Command Line shell anymore :)
 // First time you launch the compiler/debugger with F5, select 'C++ (GDB/LLDB)' and 'g++.exe' compiler.
