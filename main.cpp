f6601f138d817eeab20b07bd0079b47c03510a92
Mezdelex <alexcondegomez@gmail.com>
2021-11-19 18:10:04 +0100

    testing basic statements and similarities with Java

--- main.cpp
+++ main.cpp
@@ -9,5 +9,27 @@ main() {
   getline(cin, fullname);  // std::cin only allows us to read characters until a
                            // blank space. To be able to read more than 1 word,
                            // we need to use 'getline' from 'string' library.
-  cout << "Hello " << fullname << "!" << endl;
+
+  cout << "Enter your age:" << endl;
+  int age;
+  cin >> age;
+
+  // 'if' statement example
+  if (age >= 18)
+    cout << "Hello " << fullname << "!"
+         << " You are " << age << " years old, welcome." << endl;
+  else
+    cout << "Hello " << fullname << "!"
+         << " You are " << age << " years old, so you can't join us yet."
+         << endl;
+
+  // Same of the above using ternary operator:
+  cout << "Hello " << fullname << "!"
+       << " You are " << age << " years old, "
+       << (age >= 18 ? "welcome." : "so you can't join us yet.") << endl;
+  // Note that if we would've needed to concatenate text with other elements
+  // inside a ternary operator expression, we would've use '+' like in many
+  // other languages.
+
+  // 'switch' statement is exactly the same as in Java
 }
